47f7ca9ac9bef203a3bf9be7e6361fd6eec21c55
Brandon Haynes <bhaynes@cs.washington.edu>
2015-01-15 12:29:24 -0800

    Removed unused constants and variables from SciDB opaque scan operator

--- src/edu/washington/escience/myria/operator/SciDBOpaqueScan.java
+++ src/edu/washington/escience/myria/operator/SciDBOpaqueScan.java
@@ -4,20 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.EOFException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.DoubleBuffer;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.LittleEndianDataInputStream;
@@ -32,17 +21,6 @@ import edu.washington.escience.myria.util.SciDBChunk;
 import edu.washington.escience.myria.util.SciDBHeader;
 
 public class SciDBOpaqueScan extends LeafOperator {
-  private static final String ARRAY_PREAMBLE = "\\u0000\\u0022";
-  private static final String ARRAY_ARCHIVE_TYPE = "serialization::archive";
-  private static final int ARCHIVE_VERSION = 10;
-  private static final int HEADER_MAGIC = 0x5AC00E;
-  private static final int HEADER_VERSION = 1;
-  private static final byte HEADER_SPARSE_FLAG = 1;
-  private static final byte HEADER_RLE_FLAG = 2;
-  private static final byte HEADER_COORDINATE_MAPPING = 4;  
-  private static final byte HEADER_METADATA_FLAG = 8;
-  private static final int COORDINATE_SIZE = 8; // Number of bytes in Coordinate typedef (int64_t)
-
   /** Required for Java serialization. */
   private static final long serialVersionUID = 1L;
   /** The schema for the relation stored in this file. */
@@ -112,8 +90,7 @@ public class SciDBOpaqueScan extends LeafOperator {
 
   @Override
   protected final void cleanup() throws DbException {
-    while (buffer.numTuples() > 0)
-      buffer.popAny();
+    buffer.clear();
   }
 
   @Override
